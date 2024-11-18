d62d5da01dd09ed14b920788d172cb238d19db34
Simon Emmanuel <simonemmanuel743@gmail.com>
2024-11-18 01:41:25 +0100

    Refactoring - Fetch games with React Query

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,7 +1,6 @@
 import { useQuery } from "@tanstack/react-query";
 import genres from "../data/genres";
-import useData, { FetchResponse } from "./useData";
-import apiClient from "../services/api-client";
+import apiClient, { FetchResponse } from "../services/api-client";
 
 export interface Genre {
   id: number;
