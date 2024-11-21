f51e79af70c6e7d9d58fa4ce900bc3a9ff539b05
Simon Emmanuel <simonemmanuel743@gmail.com>
2024-11-21 21:23:01 +0100

    Build game attributes

--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -1,7 +1,7 @@
-import { Button, Heading, Spinner, Text } from "@chakra-ui/react";
-import { useState } from "react";
+import { Heading, Spinner } from "@chakra-ui/react";
 import { useParams } from "react-router-dom";
 import ExpandableText from "../components/ExpandableText";
+import GameAttributes from "../components/GameAttributes";
 import useGameDetail from "../hooks/useGameDetail";
 
 const GameDetails = () => {
@@ -13,10 +13,11 @@ const GameDetails = () => {
   if (error || !game) throw error;
 
   return (
-    <div>
+    <>
       <Heading>{game.name}</Heading>
       <ExpandableText>{game.description_raw}</ExpandableText>
-    </div>
+      <GameAttributes game={game} />
+    </>
   );
 };
 
